82a78e1c4434b1c53d12533cce1ea9c1f5fc1a71
Tiago Almeida <tyagow@hotmail.com.br>
2016-11-22 16:41:37 -0200

    corrigindo bug no template quando categoria nao possuem logo.

--- sparaguai/settings.py
+++ sparaguai/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
