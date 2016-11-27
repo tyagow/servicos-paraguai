b770e0d847c681589db8cbaa7b79bfaade0a315a
Tiago Almeida <tyagow@hotmail.com.br>
2016-11-27 13:23:26 -0200

    static files

--- sparaguai/settings.py
+++ sparaguai/settings.py
@@ -129,9 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
