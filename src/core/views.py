bbc06c037c6b8ec77dc0ca5a75a75d68ea9225ed
Tiago Almeida <tyagow@hotmail.com.br>
2016-12-07 18:17:00 -0200

    Busca por Categoria e nome de estabelecimento

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
@@ -17,14 +18,15 @@ def estabelecimento_detail(request, slug):
 
 def categoria_detail(request, slug):
     categoria = Categoria.objects.get(slug=slug)
-    estabelecimentos = Estabelecimento.objects.filter(categoria=categoria)
     return render(request, 'core/categoria_detail.html', {'categoria': categoria})
 
 
 def busca(request):
     context = {'estabelecimentos': None}
     if 's' in request.GET:
-        context = {'estabelecimentos': Estabelecimento.objects.filter(nome__icontains=request.GET['s'])}
+        query = request.GET['s']
+        query_list = Estabelecimento.objects.filter(Q(nome__icontains=query) | Q(categoria__nome__icontains=query))
+        context = {'estabelecimentos': query_list}
 
     return render(request, 'core/busca_resultado.html', context)
 
