ac881f1eb79e77c4a2db2755d5b5119870153c29
Tiago Almeida <tyagow@hotmail.com.br>
2016-12-12 23:02:55 -0200

    layout geral

--- src/api/views.py
+++ src/api/views.py
@@ -6,14 +6,11 @@ from src.core.models import Estabelecimento
 
 
 def estabelecimentos(request):
-    # cidade, nome, preco, categoria = None, None, None, None
-    # if 'cidade' in request.GET or 'nome' in request.GET or 'preco' in request.GET or 'categoria' in request.GET:
     cidade = request.GET.get('cidade', None)
     nome = request.GET.get('nome', None)
     preco = request.GET.get('preco', None)
     categoria = request.GET.get('categoria', None)
     cidade = Estabelecimento.get_cidade_index(cidade)
-    print(request.GET)
     query_filter = Estabelecimento.objects.busca(cidade=cidade, nome=nome, preco=preco, categoria=categoria)
 
     serialized = list(map(lambda obj: obj.serializer(), query_filter))
