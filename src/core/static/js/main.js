1bb2ceef33b6cd0c2cd37ca1b442c7a863f3f5bd
Tiago Almeida <tyagow@hotmail.com.br>
2017-05-19 16:06:12 -0300

    Novo layout - Template base e index fixo

--- src/core/static/js/main.js
+++ src/core/static/js/main.js
@@ -1,31 +1,34 @@
-jQuery(document).ready(function($) {
-    'use strict';
-    //SMOOTH SCROLL
-    smoothScroll.init({
-        speed: 500, // How fast to complete the scroll in milliseconds
-        easing: 'easeInOutCubic', // Easing pattern to use
-        updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
-        callbackBefore: function ( toggle, anchor ) {}, // Function to run before scrolling
-        callbackAfter: function ( toggle, anchor ) {} // Function to run after scrolling
-    });
+$(document).ready(function(){
+	$(".places").find(".itens").owlCarousel({
+		loop:true,
+		margin:20,
+		nav:false,
+		dots: false,
+		autoplay:true,
+		autoplayTimeout:2000,
+		responsive:{
+			0:{
+				items:2
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:6
+			}
+		}
+	});
+	$(".pictures").owlCarousel({
+		loop:true,
+		nav:false,
+		dots: true,
+		autoplay:true,
+		autoplayTimeout:6000,
+		items:1
+	});
 
-    //FIX HOVER EFFECT ON IOS DEVICES
-    document.addEventListener("touchstart", function(){}, true);
-});
-
-$(window).load(function(){
-
-    //HEADER ANIMATION
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        var offset = $(".header-frame").height() / 3;
-
-        if (scroll > offset) {
-            $( ".header-frame" ).addClass( "header-frame-fixed" );
-        } else {
-            $( ".header-frame" ).removeClass( "header-frame-fixed" );
-        }
-
-    });
+	$("#rateYo").rateYo({
+	   rating: 3.6
+	 });
 
 });
