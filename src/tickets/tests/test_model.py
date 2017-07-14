6b7d2c097164f5f3b3f54217b21df42d7ac32d5c
Tiago Almeida <tyagow@hotmail.com.br>
2017-07-13 21:00:05 -0300

    Removendo testes desatualizados

--- src/tickets/tests/test_model.py
+++ src/tickets/tests/test_model.py
@@ -3,7 +3,6 @@ from django.core import urlresolvers
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from src.core.models import Empresa
 from src.tickets.models import Ticket, TIPO_CHOICES, STATUS_CHOICES
 
 
@@ -13,34 +12,12 @@ def assertContents(self, contents):
             self.assertContains(self.response, expected_content)
 
 
-class TicketModelManagerTest(TestCase):
-    fixtures = ['empresas.json', 'users.json']
-
-    def test_ticket_representante(self):
-        empresa = Empresa.objects.first()
-        user = User.objects.first()
-        ticket = Ticket.objects.representante_empresa(empresa=empresa, user=user)
-        self.assertTrue(Ticket.objects.exists())
-
-    def test_ticket_cadastro_nova_empresa(self):
-        user = User.objects.first()
-        Ticket.objects.cadastro_empresa(nome='Tim', cidade='Ciudad del Este', endereco='Rua da Silva, 155', user=user)
-        ticket = Ticket.objects.filter(tipo=TIPO_CHOICES[1][0]).first()
-        self.assertEqual(1, ticket.tipo)
-
-
 class TicketModelTest(TestCase):
     """Tickets """
     def setUp(self):
         self.user = User.objects.create_superuser(username='jacob', password='top_secret', email='tyow@hotmail.com.br')
         self.client.login(username=self.user.username, password='top_secret')
         self.ticket = Ticket.objects.create(criador=self.user, object_id=1)
-        self.ticket2 = Ticket.objects.cadastro_empresa(
-            nome='Partiu',
-            cidade='Ciudad del Este',
-            endereco='Rua da Silva, 155',
-            user=self.user
-        )
 
     def test_create(self):
         self.assertTrue(Ticket.objects.exists())
