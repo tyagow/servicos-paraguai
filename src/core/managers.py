b5b4adc28a2352a9836283c2361ff5d109eba2f3
Tiago Almeida <tyagow@hotmail.com.br>
2017-07-15 18:35:22 -0300

    Queryset ajustando novos filtros

--- src/core/managers.py
+++ src/core/managers.py
@@ -39,30 +39,71 @@ class CategoriaManager(models.Manager):
         return len(query) > 0
 
 
-class EstabelecimentoManager(models.Manager):
-    def busca(self, *args, nome=None, cidade=None, preco=None, categoria=None, **kwargs):
-        queryFilter = super(EstabelecimentoManager, self).all()
+class EstabelecimentoQuerySet(models.QuerySet):
+    def busca(self, nome=None, cidade=None, preco=None, categoria=None):
+        queryFilter = self.all()
         if cidade and not 'todas' in cidade:
-            queryFilter = super(EstabelecimentoManager, self).filter(cidade=cidade)
+            queryFilter = self.filter(cidade=cidade)
 
         if categoria and not 'todas' in categoria:
             if queryFilter:
                 queryFilter = queryFilter.filter(categoria__nome__icontains=categoria)
             else:
-                queryFilter = super(EstabelecimentoManager, self).filter(categoria__nome__icontains=categoria)
+                queryFilter = self.filter(categoria__nome__icontains=categoria)
 
         if preco:
             if queryFilter:
                 queryFilter = queryFilter.filter(preco__valor__lte=Decimal(preco))
             else:
-                queryFilter = super(EstabelecimentoManager, self).filter(preco__valor__lte=Decimal(preco))
+                queryFilter = self.filter(preco__valor__lte=Decimal(preco))
 
         if nome:
             if queryFilter:
                 queryFilter = queryFilter.filter(nome__icontains=nome)
             else:
-                queryFilter = super(EstabelecimentoManager, self).filter(nome__icontains=nome)
+                queryFilter = self.filter(nome__icontains=nome)
         return queryFilter.distinct()
 
-    def recomendados(self, *args, **kwargs):
-        return super(EstabelecimentoManager, self).filter(recomendado=True)
+    def recomendados(self, count=None):
+        total = self.count()
+        if not count:
+            return self.filter(recomendado=True)
+        elif count > total:
+            dif = count - total
+            principais = list(self.all())
+            index = self.aggregate(count=Count('id'))['count']
+            while dif > 0:
+                random_index = randint(0, index - 1)
+                principais.append(self.all()[random_index])
+                dif -= 1
+            return principais
+        else:
+            index = self.aggregate(count=Count('id'))['count']
+            random_index = randint(0, index - 1)
+            if random_index + count >= index:
+                random_index = index - count
+            return self.filter(recomendado=True)[random_index:random_index + count]
+
+    def mais_buscados(self, count=None):
+        total = self.count()
+        self = self.order_by('-search_hits')
+        if not count:
+            return self.all()
+        elif count > total:
+            dif = count - total
+            principais = list(self.all())
+            index = self.aggregate(count=Count('id'))['count']
+            while dif > 0:
+                random_index = randint(0, index - 1)
+                principais.append(self.all()[random_index])
+                dif -= 1
+            return principais
+        else:
+            index = self.aggregate(count=Count('id'))['count']
+            random_index = randint(0, index - 1)
+            if random_index + count >= index:
+                random_index = index - count
+            return self[random_index:random_index + count]
+
+
+EstabelecimentoManager = models.Manager.from_queryset(EstabelecimentoQuerySet)
