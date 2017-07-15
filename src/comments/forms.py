f8fca457995c92a7f86d729a6d1adfb28b0c7a97
Tiago Almeida <tyagow@hotmail.com.br>
2017-07-14 22:12:12 -0300

    Comment form nota nao requirido

--- src/comments/forms.py
+++ src/comments/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext as _
 class CommentForm(forms.Form):
     content_type = forms.CharField(widget=forms.HiddenInput)
     object_id = forms.IntegerField(widget=forms.HiddenInput)
-    nota = forms.IntegerField(widget=forms.HiddenInput)
+    nota = forms.IntegerField(widget=forms.HiddenInput, required=False)
     content = forms.CharField(
         label='',
         help_text=_('Escreva seu comentario'),
