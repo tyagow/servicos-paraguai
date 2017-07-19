68ca44744979f81337391901080a74951b645fb3
Tiago Almeida <tyagow@hotmail.com.br>
2017-07-19 19:00:36 -0300

    Formulario Post imagem obrigatiria

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -10,7 +10,7 @@ class PostForm(forms.ModelForm):
     title = forms.CharField(label='Titulo')
     content = forms.CharField(label='Conteudo', widget=PagedownWidget(show_preview=False))
     publish = forms.DateField(label='Publicar em', widget=forms.SelectDateWidget)
-    image = forms.ImageField(label='Capa do post', help_text='TAMANHO DA IMAGEM PRECISA SER 555x235 PARA NOTICIA OU 265x360 PARA LAZER / TURISMO')
+    image = forms.ImageField(label='Capa do post', required=True, help_text='TAMANHO DA IMAGEM PRECISA SER 555x235 PARA NOTICIA OU 265x360 PARA LAZER / TURISMO')
     draft = forms.BooleanField(label='Rascunho', required=False)
     # type = forms.IntegerField(label='Categoria')
 
